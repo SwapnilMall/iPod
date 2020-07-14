044b86f23cb811971cd825c7c9e1ff6ee7759871
Swapnil Mall <swapnilmall03@gmail.com>
2020-07-14 22:02:38 +0530

    remaining file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import './index.css';
-import Wheel from './Wheel';
-import ZingTouch from 'zingtouch';
+import './App.css';
+import Buttons from './Buttons';
 import Screen from './Screen';
+import ZingTouch from 'zingtouch';
+import 'lodash';
 import $ from 'jquery';
 
 class App extends React.Component {
@@ -12,9 +13,14 @@ class App extends React.Component {
 		this.temp_selected = 0;
 		this.state = {
 			options: [ 'Games', 'Music', 'Settings', 'CoverFlow' ],
-			songs_Menu: [ 'All Songs', 'Artists', 'Albums' ],
+			change_in_angle: 0,
+			selected: 0,
 			showPage: -1,
-			selected: 0
+			general_menu: [ 'Games', 'Music', 'Settings', 'Cover Flow' ],
+			songs_sub_menu: [ 'All Songs', 'Artists', 'Albums' ],
+			current_music_selection: 0,
+			song_index: -1,
+			currently_on_play_music_screen: false
 		};
 	}
 
@@ -50,26 +56,219 @@ class App extends React.Component {
 	}
 
 	menuButtonClicked = () => {
+		/* if (this.state.options === this.state.songs_sub_menu)
+        {
+            this.setState({
+                options: this.state.general_menu
+            });
+            return;
+        } */
+
 		let screenMenuClassList = document.getElementsByClassName('screen-menu')[0].classList;
 		if (screenMenuClassList.contains('width-50')) {
-			document.getElementsByClassName('screen-menu').classList.remove('width-50');
-			// $('screen-menu').removeClass('width-50'); //hide menu
-			console.log('ok');
+			$('.screen-menu').removeClass('width-50'); //hide menu
 		} else {
-			// $('screen-menu').addClass('width-50'); //show menu
-			console.log('not');
-			document.getElementsByClassName('screen-menu').classList.add('width-50');
+			$('.screen-menu').addClass('width-50'); //show menu
 		}
 	};
+
+	selectButtonClicked = () => {
+		if (this.state.selected === 1 && this.state.options.length === 4) {
+			this.setState({
+				options: this.state.songs_sub_menu,
+				selected: 0,
+				showPage: -1,
+				song_index: -1 //we dont want to play any song
+			});
+			this.temp_selected = 0;
+			return;
+		}
+		if (!document.getElementsByClassName('screen-menu')[0].classList.contains('width-50')) {
+			//side menu is not visible
+			if (this.state.options.length === 3) {
+				//I must be on the music section
+				if (this.state.showPage === 0) {
+					//I am on all songs page
+					if (this.state.song_index === -1) {
+						//we are not on the music page
+						this.setState({
+							song_index: this.state.current_music_selection //which song to play (here we want to play a song)
+						});
+						this.temp_selected = 0;
+						return;
+					}
+				}
+			}
+		}
+		this.setState({
+			showPage: this.state.selected,
+			song_index: -1, //we dont want to play any song
+			selected: 0
+		});
+		this.temp_selected = 0;
+		this.menuButtonClicked();
+	};
+
+	leftButtonClicked = () => {
+		/* the left button could have also been clicked to change the song which is currently being played to the previous song. */
+		if (this.state.currently_on_play_music_screen) {
+			//if i am on the play music screen
+			if (!document.getElementsByClassName('screen-menu')[0].classList.contains('width-50')) {
+				//if the menu is not present on the screen
+				if ($('#audio')[0] !== undefined) {
+					/* handling the turning off of button lights when i play the next song  */
+					$('.buttons-container').removeClass('colored');
+				}
+				//here i can switch to next song
+				if (this.state.song_index === 0) {
+					this.setState({
+						song_index: 5
+					});
+					return;
+				}
+				if (this.state.song_index !== -1) {
+					this.setState({
+						song_index: this.state.song_index - 1
+					});
+					return;
+				}
+			}
+		}
+
+		if (
+			this.state.options.length === 3 &&
+			document.getElementsByClassName('screen-menu')[0].classList.contains('width-50') //if the menu is open and it is on the songs page only then if the left button clicked, menu will be changed to general options
+		)
+			this.setState({
+				options: this.state.general_menu,
+				song_index: -1,
+				selected: 0
+			});
+		if (!document.getElementsByClassName('screen-menu')[0].classList.contains('width-50')) {
+			//side menu is not visible
+			if (this.state.options.length === 3) {
+				//I must be on the music section
+				if (this.state.showPage === 0) {
+					//I am on all songs page
+					if (
+						this.state.current_music_selection === 0 //If I am playing the music at 5th index then I will need to reduce the index to 0 on next right button click.
+					)
+						this.setState({
+							current_music_selection: 5,
+							song_index: -1
+						});
+					else
+						this.setState({
+							current_music_selection: this.state.current_music_selection - 1,
+							song_index: -1
+						});
+				}
+			}
+		}
+	};
+
+	rightButtonClicked = () => {
+		/* the right button could have also been clicked to change the song which is currently being played to the next song. */
+		if (this.state.currently_on_play_music_screen) {
+			//if i am on the play music screen
+			if (!document.getElementsByClassName('screen-menu')[0].classList.contains('width-50')) {
+				//if the menu is not present on the screen
+				if ($('#audio')[0] !== undefined) {
+					/* handling the turning off of button lights when i play the next song  */
+					$('.buttons-container').removeClass('colored');
+				}
+				//here i can switch to next song
+				if (this.state.song_index === 5) {
+					this.setState({
+						song_index: 0
+					});
+					return;
+				}
+				if (this.state.song_index !== -1) {
+					this.setState({
+						song_index: this.state.song_index + 1
+					});
+					return;
+				}
+			}
+		}
+		if (!document.getElementsByClassName('screen-menu')[0].classList.contains('width-50')) {
+			//side menu is not visible
+			if (this.state.options.length === 3) {
+				//I must be on the music section
+				if (this.state.showPage === 0) {
+					//I am on all songs page
+					if (
+						this.state.current_music_selection === 5 //If I am playing the music at 5th index then I will need to reduce the index to 0 on next right button click.
+					)
+						this.setState({
+							current_music_selection: 0
+						});
+					else
+						this.setState({
+							current_music_selection: this.state.current_music_selection + 1
+						});
+				}
+			}
+		}
+	};
+
+	currentlyOnPlayMusicScreen = () => {
+		if (this.state.currently_on_play_music_screen) {
+			$('.buttons-container').removeClass('colored');
+			this.setState({
+				currently_on_play_music_screen: false
+			});
+		} else
+			this.setState({
+				currently_on_play_music_screen: true
+			});
+	};
+
+	playPauseButtonClicked = () => {
+		if ($('#audio')[0] !== undefined) {
+			if ($('#audio')[0].paused) {
+				//if the music is paused i will play it, also turn on the button lights
+				$('#audio')[0].play();
+				$('.buttons-container').addClass('colored');
+				return;
+			}
+			$('#audio')[0].pause();
+			$('.buttons-container').removeClass('colored');
+		}
+	};
+
+	rotatePod = () => {
+		/* screen rotation feature */
+		$('.App').toggleClass('rotate-anti-clockwise');
+		$('.buttons-container').toggleClass('rotate-clockwise');
+		$('.screen-container').toggleClass('rotate-clockwise');
+	};
+
 	render() {
 		return (
 			<div className="App">
 				<Screen
-					optionsInMenu={this.state.options}
 					selectedOption={this.state.selected}
 					showPage={this.state.showPage}
+					optionsInMenu={this.state.options}
+					currentMusicSelection={this.state.current_music_selection}
+					songIndex={this.state.song_index}
+					currentlyOnPlayMusicScreen={this.currentlyOnPlayMusicScreen}
+					playPauseButtonClicked={this.playPauseButtonClicked}
 				/>
-				<Wheel menuButtonClicked={this.menuButtonClicked} />
+				<Buttons
+					check={this.checker}
+					centerButton={this.centerButton}
+					menuButtonClicked={this.menuButtonClicked}
+					selectButtonClicked={this.selectButtonClicked}
+					leftButtonClicked={this.leftButtonClicked}
+					rightButtonClicked={this.rightButtonClicked}
+					playPauseButtonClicked={this.playPauseButtonClicked}
+				/>
+				<button className="rotate" onClick={this.rotatePod}>
+					<i className="fas fa-undo" />
+				</button>
 			</div>
 		);
 	}
--- src/Screen.js
+++ src/Screen.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import Menu from './ScreenComponents/Menu';
-import Games from './ScreenComponents/Games';
-import Setting from './ScreenComponents/Setting';
-import CoverFlow from './ScreenComponents/CoverFlow';
+import Menu from './Screens/Menu';
+import Game from './Screens/Games';
+import Setting from './Screens/Setting';
+import Coverflow from './Screens/Coverflow';
+/* music */
+import AllSongs from './Screens/AllSongs';
+import Artists from './Screens/Artists';
+import Albums from './Screens/Albums';
 
-const Screen = (props) => {
-	return (
-		<div className="screen-container">
-			<Menu selectedOption={props.selectedOption} optionsInMenu={props.optionsInMenu} />
-			{props.showPage === 0 && props.optionsInMenu.length === 4 ? <Games /> : ''}
-			{props.showPage === 2 && props.optionsInMenu.length === 4 ? <Setting /> : ''}
-			{props.showPage === 3 && props.optionsInMenu.length === 4 ? <CoverFlow /> : ''}
-		</div>
-	);
-};
+class Screen extends React.Component
+{
+    render()
+    {
+        return (
+            <div className="screen-container">
+                <Menu
+                    selectedOption={this.props.selectedOption}
+                    optionsInMenu={this.props.optionsInMenu}
+                />
+                {this.props.showPage === 0 && this.props.optionsInMenu.length === 4 ? <Game /> : ''}
+                {this.props.showPage === 2 && this.props.optionsInMenu.length === 4 ? <Setting /> : ''}
+                {this.props.showPage === 3 && this.props.optionsInMenu.length === 4 ? <Coverflow /> : ''}
+
+                {this.props.showPage === 0 && this.props.optionsInMenu.length === 3 ? <AllSongs
+                currentMusicSelection={this.props.currentMusicSelection}
+                songIndex={this.props.songIndex}
+                currentlyOnPlayMusicScreen={this.props.currentlyOnPlayMusicScreen}
+                playPauseButtonClicked={this.props.playPauseButtonClicked}
+                /> : ''}
+                {this.props.showPage === 1 && this.props.optionsInMenu.length === 3 ? <Artists /> : ''}
+                {this.props.showPage === 2 && this.props.optionsInMenu.length === 3 ? <Albums /> : ''}
+            </div>
+        );
+    }
+
+}
 
 export default Screen;
